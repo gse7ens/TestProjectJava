8cd3c45b58f00074ea520b0c1fa21262a72df6f5
Alex Ander <gse7ens@gmail.com>
2022-11-10 20:28:20 +0200

    added palindrome method

--- src/mypackage/TestClass.java
+++ src/mypackage/TestClass.java
@@ -2,15 +2,29 @@ package mypackage;
 
 public class TestClass {
     public static void main(String[] args) {
-        System.out.println(reverseText("Reverse me", ""));
-
+        String text = "Revert me";
+        String text2 = "tenet";
+        System.out.println(text + " backward is: " + reverseText(text));
+        System.out.println("Is " + text2 + " palindrome? " + isPalindrome("tenet"));
     }
 
-    static String reverseText(String text, String reversedText){
+// method for reversing string
+    static String reverseText(String text){
+        String reversedText = "";
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            reversedText = ch + reversedText;
+        }
+        return reversedText;
+    }
+
+// method for checking if a string is palindrome or not
+    static boolean isPalindrome(String text){
+        String reversedText = "";
         for (int i = 0; i < text.length(); i++) {
             char ch = text.charAt(i);
             reversedText= ch + reversedText;
         }
-        return reversedText;
-    } //method for reversing string
+        return text.equals(reversedText);
+    }
 }
